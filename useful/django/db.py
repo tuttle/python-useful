4d801c99ceb74baccd98151e400295d5906375a1
Vlada Macek <macek@sandbox.cz>
2012-12-29 19:25:19 +0100

    Added django db utils.

---
+++ useful/django/db.py
@@ -0,0 +1,17 @@
+from django.db import connection
+
+def truncate_table(model_or_tablename, cascade=False, cursor=None):
+    if cursor is None:
+        cursor = connection.cursor()
+
+    if hasattr(model_or_tablename, '_meta'):
+        tablename = model_or_tablename._meta.db_table
+    else:
+        tablename = model_or_tablename
+
+    if cascade:
+        stmt = 'TRUNCATE %s CASCADE'
+    else:
+        stmt = 'TRUNCATE %s'
+
+    cursor.execute(stmt % connection.ops.quote_name(tablename))
