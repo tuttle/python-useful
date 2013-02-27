e40992ec9e616419ba93320b62d73790befe4f97
Vlada Macek <macek@sandbox.cz>
2013-02-27 15:35:36 +0100

    Added missing package data spec to setup.py.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import setup, find_packages
 setup(name='useful',
       version='0.2',
       packages=find_packages(),
+      package_data={'useful': ['django/locale/cs/LC_MESSAGES/django.*']},
       description = "Everyday use utilities for writing Python/Django apps",
       author = "Vlada Macek",
       author_email = "macek@sandbox.cz",
