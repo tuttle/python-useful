7a39a0dac23eff6214a2c185594585bc17e852dd
Vlada Macek <macek@sandbox.cz>
2013-11-22 15:39:16 +0100

    Added get_intervals_overlap handy function.

--- useful/shortcuts.py
+++ useful/shortcuts.py
@@ -10,3 +10,31 @@ def int_or_None(value):
         return int(value)
     except (ValueError, TypeError):
         return None
+
+
+def get_intervals_overlap(a, b):
+    """
+    >>> get_overlap([0,  5], [10, 20])
+    -5
+    >>> get_overlap([0,  9], [10, 20])
+    -1
+    >>> get_overlap([0,  10], [10, 20])
+    0
+    >>> get_overlap([0,  15], [10, 20])
+    5
+    >>> get_overlap([10,  15], [10, 20])
+    5
+    >>> get_overlap([10,  20], [10, 20])
+    10
+    >>> get_overlap([15,  20], [10, 20])
+    5
+    >>> get_overlap([15,  15], [10, 20])
+    0
+    >>> get_overlap([20, 20], [10, 20])
+    0
+    >>> get_overlap([20, 20], [10, 20])
+    0
+    >>> get_overlap([30, 40], [10, 20])
+    -10
+    """
+    return min(a[1], b[1]) - max(a[0], b[0])
