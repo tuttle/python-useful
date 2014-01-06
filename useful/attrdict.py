5b1f23c0fd12339433479435ce877d24528939fc
Vlada Macek <macek@sandbox.cz>
2014-01-06 23:57:03 +0100

    Added OrderedAttrDict class.

--- useful/attrdict.py
+++ useful/attrdict.py
@@ -16,3 +16,40 @@ class AttrDict(dict):
         self[name] = value
 
     __getattr__ = dict.__getitem__
+
+
+try:
+    from collections import OrderedDict
+except ImportError:
+    pass
+else:
+    class OrderedAttrDictSetCollision(Exception):
+        """
+        Raised when the OrderedAttrDict refuses to change any OrderedDict's attribute.
+        """
+
+    class OrderedAttrDict(OrderedDict):
+        """
+        Like AttrDict, but remembers ordering in which values were set.
+        """
+        def __setattr__(self, name, value):
+            """
+            Needs a fork to allow OrderedDict to establish some private names in constructor.
+            """
+            if name.startswith('_OrderedDict__'):
+                return super(OrderedAttrDict, self).__setattr__(name, value)
+
+            if hasattr(OrderedDict, name):
+                raise OrderedAttrDictSetCollision(name)
+
+            self[name] = value
+
+        def __getattr__(self, name):
+            """
+            While AttrDict.__getattr__ is raising KeyError, this one needs to convert it to
+            AttributeError as OrderedDict establishes some names in constructor.
+            """
+            try:
+                return self.__getitem__(name)
+            except KeyError, e:
+                raise AttributeError(e)
