77b84b9de59c13ddd7812fb5eb02483d4d8f6fd2
Vitek Pliska <vitek@creatiweb.cz>
2014-11-23 14:45:56 +0100

    Packaging/release on pypi.

--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
-#!/usr/bin/env python
-from setuptools import setup, find_packages
+from os.path import dirname, join
+from setuptools import setup
 
-# I'm pretty unexperienced in writing setup.py. Will welcome advices, thanks.
+import useful
 
-setup(name='useful',
-      version='0.5',
-      packages=find_packages(),
-      package_data={'useful': ['django/locale/cs/LC_MESSAGES/django.*']},
-      description = "Everyday use utilities for writing Python/Django apps",
-      author = "Vlada Macek",
-      author_email = "macek@sandbox.cz",
-      url = "https://github.com/tuttle/python-useful",
+with open(join(dirname(__file__), 'README.md')) as readme_file:
+    long_description = readme_file.read()
+
+setup(
+	name='useful',
+	version=useful.__versionstr__,
+	packages=('useful',),
+	package_data={'useful': ['django/locale/cs/LC_MESSAGES/django.*']},
+	description="Everyday use utilities for writing Python/Django apps",
+	long_description=long_description,
+	author="Vlada Macek",
+	author_email="macek@sandbox.cz",
+	url="https://github.com/tuttle/python-useful",
+	license='BSD License',
+
+    zip_safe=False,
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: WWW",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Topic :: Software Development :: Libraries",
+    ],
 )
---
+++ useful/__init__.py
@@ -0,0 +1,2 @@
+__version__ = (0, 6, 0)
+__versionstr__ = '.'.join(map(str, __version__))
