03ed35fd2735f1c5e43548ac3786dbcf4a262f30
Vlada Macek <macek@sandbox.cz>
2015-03-06 15:29:25 +0100

    Added support for lock stealing to FileLock.

--- useful/filelock.py
+++ useful/filelock.py
@@ -31,8 +31,12 @@ class FileLock(object):
             def func1():
                 print "This function will only be executed by a single thread at the same time."
 
+        Also the lock stealing is implemented (stealing=True), but only on the POSIX system where it is possible to send
+        signal 0 to process to test its existence. If requested, the locking process will write a small JSON info to the
+        lockfile. Each time before waiting for lock to be released, the competing process signals the lock originator
+        by PID. If the locking process does not exist, the lock file is removed and acquiring is tried again.
     """
-    def __init__(self, file_name, timeout=30, delay=.15):
+    def __init__(self, file_name, timeout=30, delay=0.2, stealing=False):
         """ Prepare the file locker. Specify the file to lock and optionally
             the maximum timeout and the delay between each attempt to lock.
         """
@@ -41,6 +45,14 @@ class FileLock(object):
         self.file_name = file_name
         self.timeout = timeout
         self.delay = delay
+        self.stealing = stealing
+
+        if stealing:
+            if os.name != 'posix':
+                raise RuntimeError("Detecting a running process by its PID is only supported on a POSIX system.")
+
+            import json
+            self.json = json
 
     def acquire(self):
         """ Acquire the lock, if possible. If the lock is in use, it check again
@@ -56,10 +68,29 @@ class FileLock(object):
             except OSError as e:
                 if e.errno != errno.EEXIST:
                     raise
+
+                if self.should_steal():
+                    os.unlink(self.lockfile)
+                    continue
+
                 if (time.time() - start_time) >= self.timeout:
                     raise FileLockTimeoutException("%d seconds passed." % self.timeout)
+
                 time.sleep(self.delay)
+
         self.is_locked = True
+
+        if self.stealing:
+            import datetime
+            import sys
+
+            info = {
+                'lock_time': datetime.datetime.now().isoformat(),  # warning: timezone unaware!
+                'pid': os.getpid(),
+                'argv': sys.argv,
+            }
+            os.write(self.fd, self.json.dumps(info, indent=4))
+            os.fsync(self.fd)
 
     def release(self):
         """ Get rid of the lock by deleting the lockfile.
@@ -70,6 +101,25 @@ class FileLock(object):
             os.close(self.fd)
             os.unlink(self.lockfile)
             self.is_locked = False
+
+    def should_steal(self):
+        if self.stealing:
+            try:
+                info = self.json.load(open(self.lockfile))
+            except ValueError:
+                # We don't steal if the lockfile is empty of contains an invalid JSON value.
+                pass
+            else:
+                try:
+                    # POSIX test whether the given PID exists
+                    os.kill(info['pid'], 0)
+                except OSError as e:
+                    # For EPERM we intentionally don't steal as the locking process might have higher privilege.
+                    # ESRCH means "No such process".
+                    if e.errno == errno.ESRCH:
+                        return True
+
+        return False
 
     def __enter__(self):
         """ Activated when used in the with statement.
